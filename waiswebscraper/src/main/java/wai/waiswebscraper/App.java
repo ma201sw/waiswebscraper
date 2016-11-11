7f536907bcdb26d1ba8ae241a66468cb9157b283
ma201sw <shauwong@hotmail.com>
2016-11-11 15:40:54 +0000

    init

---
+++ waiswebscraper/src/main/java/wai/waiswebscraper/App.java
@@ -0,0 +1,13 @@
+package wai.waiswebscraper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
