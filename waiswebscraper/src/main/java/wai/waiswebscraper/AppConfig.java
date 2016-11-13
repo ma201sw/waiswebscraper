96b38e5d4a6f8ebcd8a15a91b494be89ba43fe43
ma201sw <shauwong@hotmail.com>
2016-11-13 20:29:56 +0000

    t

---
+++ waiswebscraper/src/main/java/wai/waiswebscraper/AppConfig.java
@@ -0,0 +1,15 @@
+package wai.waiswebscraper;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+
+/*	@Bean
+	public TestService testService() {
+	    System.out.println("ser from bean");
+	    return new TestService();
+	}*/
+}
