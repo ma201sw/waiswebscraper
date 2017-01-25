9b981342e38866b1a8a74c4ce1c6a6ca734c2f74
ma201sw <shauwong@hotmail.com>
2017-01-25 13:41:28 +0000

    Update ScraperController.java

--- waiswebscraper/src/main/java/wai/waiswebscraper/controllers/ScraperController.java
+++ waiswebscraper/src/main/java/wai/waiswebscraper/controllers/ScraperController.java
@@ -36,8 +36,8 @@ public class ScraperController {
     public String result() {
     	try {
     		//return json string of the products
-    		//return productListService.productListJson("http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html");
-		return productListService.productListJson("http://www.sainsburys.co.uk/shop/gb/groceries/fruit-veg/ripe---ready#langId=44&storeId=10151&catalogId=10241&categoryId=185749&parent_category_rn=12518&top_category=12518&pageSize=20&orderBy=FAVOURITES_ONLY%7CTOP_SELLERS&searchTerm=&beginIndex=0");
+    		return productListService.productListJson("http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html");
+		//return productListService.productListJson("http://www.sainsburys.co.uk/shop/gb/groceries/fruit-veg/ripe---ready#langId=44&storeId=10151&catalogId=10241&categoryId=185749&parent_category_rn=12518&top_category=12518&pageSize=20&orderBy=FAVOURITES_ONLY%7CTOP_SELLERS&searchTerm=&beginIndex=0");
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
